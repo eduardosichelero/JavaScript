52356fe808f156b11b9c43710ec0cf18d87fd244
Eduardo da Silva Sichelero <eduardoddssichelero@gmail.com>
2023-09-06 16:34:44 -0300

    att-03

---
+++ Exercicios/ex005/ex001.js
@@ -0,0 +1,13 @@
+
+
+
+
+var idade = 5
+console.log(`Você tem ${idade}.`)
+if (idade < 16) {
+    console.log(" Não Vota")
+} else if (idade < 18) {
+    console.log("Voto Opcional")
+} else {
+    console.log("Voto Obigatório!")
+}
