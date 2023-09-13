15c038d56797ff43f44130251cfa5fd4773ee194
Eduardo da Silva Sichelero <eduardoddssichelero@gmail.com>
2023-09-13 19:49:48 -0300

    atividade-01

--- Atividades/Atividade001/script.js
+++ Atividades/Atividade001/script.js
@@ -1,1 +1,19 @@
-document.write()
+function carregar() {
+    var msg = document.getElementById('msg');
+    var imagem = document.getElementById('imagem-dia-noite');
+    var data = new Date();
+    var hora = data.getHours();
+
+    msg.innerHTML = `Agora são ${hora} horas.`;
+
+    if (hora >= 0 && hora < 12) {
+        imagem.src = 'manha.png';
+        document.body.style.background = '#E6BD4F'
+    } else if (hora >= 12 && hora < 18) {
+        imagem.src = 'pordosol.png';
+        document.body.style.background = '#F25C05'
+    } else {
+        imagem.src = 'noite.png'
+        document.body.style.background = '#647677'
+    }
+}
